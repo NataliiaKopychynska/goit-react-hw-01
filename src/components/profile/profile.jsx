10921853c641884606c2829786d8ad28a0d4b5f0
Nataliia <nkopichinckaya@gmail.com>
2024-12-07 09:23:25 +0100

    final first task

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -12,15 +12,15 @@ export default function Profile({ username, tag, location, avatar, stats }) {
       <ul className={styles.ulStats}>
         <li className={styles.liStats}>
           <span>Followers</span>
-          <span className={stats.spanLi}>{stats.followers}</span>
+          <span className={styles.spanLi}>{stats.followers}</span>
         </li>
         <li className={styles.liStats}>
           <span>Views</span>
-          <span className={stats.spanLi}>{stats.views}</span>
+          <span className={styles.spanLi}>{stats.views}</span>
         </li>
         <li className={styles.liStats}>
           <span>Likes</span>
-          <span className={stats.spanLi}>{stats.likes}</span>
+          <span className={styles.spanLi}>{stats.likes}</span>
         </li>
       </ul>
     </div>
